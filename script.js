a5c7c3a7849c610269d40d508c288e077c658263
Elijah McDaniels <elijahmcdaniels@R2Hs-MacBook-Pro.local>
2025-07-01 10:52:22 -0400

    Delete button

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
-// This function allows the user to create a new task and adds it to the existing list
+// This function allows the user to create a new task
 function addNewTask() {
     const currentTasks = document.getElementById("tasklist"); // Takes the existing tasks
     const newListItem = document.createElement("li"); // Creates a new list item
     let inputValue = document.getElementById("taskInput").value; // Takes value of input box
+    
     let text = document.createTextNode(inputValue);
     let newTask = newListItem.appendChild(text); // Appends value of input box to list item
+    
     currentTasks.appendChild(newListItem);
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    newListItem.appendChild(deleteButton)
 }
 
 const button = document.getElementById("addButton")
 button.addEventListener("click", addNewTask); 
-
-// This function adds a delete button for each task
-function addDeleteButton() {
-    let allTasks = document.getElementsByTagName("li");
-    for(i = 0; i < allTasks.length; i++) {
-        const deleteButton = document.createElement("button");
-        deleteButton.textContent = "Delete";
-        const span = document.createElement("span");
-        span.appendChild(deleteButton);
-        allTasks[i].appendChild(span);
-    }
-}
