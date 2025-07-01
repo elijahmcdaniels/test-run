8830ebdde35bfafa8c44bbd626d880ae160c6b68
Elijah McDaniels <elijahmcdaniels@R2Hs-MacBook-Pro.local>
2025-07-01 08:55:39 -0400

    Trying to fix add task function

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 // This function allows the user to create a new task and adds it to the existing list
 
-const currentTasks = document.getElementById("tasklist"); // Takes the existing tasks
-const newListItem = document.createElement("li"); // Creates a new list item
+
 
 function addNewTask() {
-    let inputValue = currentTasks.value; // Takes value of input box
-    let newTask = newListItem.appendChild(inputValue); // Appends value of input box to list item
+    const currentTasks = document.getElementById("tasklist"); // Takes the existing tasks
+    const newListItem = document.createElement("li"); // Creates a new list item
+    let inputValue = document.getElementById("taskInput").value; // Takes value of input box
+    let text = document.createTextNode(inputValue);
+    let newTask = newListItem.appendChild(text); // Appends value of input box to list item
     currentTasks.appendChild(newTask)
 }
 
 const button = document.getElementById("addButton")
-button.addEventListener("click", addNewTask);
+button.addEventListener("click", addNewTask); 
